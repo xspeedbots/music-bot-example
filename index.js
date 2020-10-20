214567b62cf5c1b3d45017b4bf30d3fd9adfdae1
AnnasSlin <52790211+AnnasSlin@users.noreply.github.com>
2020-10-20 20:24:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PREFIX = process.env.PREFIX;
 const youtube = new YouTube(process.env.YTAPI_KEY);
 const queue = new Map();
 bot.on('ready', () => {
-  bot.user.setActivity('+help X-SPEED MUSIC ', { type: 'LISTENING' })
+  bot.user.setActivity('+help X-SPEED MUSIC ', { type: 'PLAYING' })
 })
 bot.on("warn", console.warn);
 bot.on("error", console.error);
