ec3a455b1d68f63e4db92bc0f41ff7a3efea7a88
AnnasSlin <52790211+AnnasSlin@users.noreply.github.com>
2020-10-21 20:39:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PREFIX = process.env.PREFIX;
 const youtube = new YouTube(process.env.YTAPI_KEY);
 const queue = new Map();
 bot.on('ready', () => {
-  bot.user.setActivity(`Iam in ${client.guilds.cache.size} servers`, { type: 'STREAMING' })
+  bot.user.setActivity(`Iam in ${client.guilds.cache.size} servers`, { type: 'WATCHING' })
 });
 bot.on("warn", console.warn);
 bot.on("error", console.error);
